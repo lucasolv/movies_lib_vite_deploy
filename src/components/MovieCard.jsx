efdd76a6f107fe7665954ec7a54a2bdf9482085e
iesblucasolv <lucas.o.ferreira@iesb.edu.br>
2024-10-15 14:17:46 -0300

    subindo projeto pro github

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,8 +1,8 @@
 import { Link } from "react-router-dom"
 import {FaStar} from 'react-icons/fa'
 
-//const imageUrl = import.meta.env.VITE_IMG
-const imageUrl = "https://image.tmdb.org/t/p/w500/"
+const imageUrl = import.meta.env.VITE_IMG
+/* const imageUrl = "https://image.tmdb.org/t/p/w500/" */
 
 const MovieCard = ({movie, showLink=true}) => {
   return (
